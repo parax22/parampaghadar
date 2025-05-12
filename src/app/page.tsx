49333a49081dd700c3a97e49878d57ec9b56e7b9
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2025-05-12 18:15:57 +0530

    + setup

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Hero from "@/components/home/hero";
+import WorkExperience from "@/components/home/work-experience";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <WorkExperience />
+    </>
+  );
+}
