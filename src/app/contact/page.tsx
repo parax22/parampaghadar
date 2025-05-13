e1528f372f65c06babababcc9296758357ed121c
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2025-05-13 16:07:43 +0530

    Create page.tsx

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,17 @@
+import Contact from "@/components/home/contact";
+import { Metadata } from "next";
+import React from "react";
+
+export const metadata: Metadata = {
+  title: "Contact | DevsPro",
+  description: "Created by devsloka.in ",
+};
+const ContactPage = () => {
+  return (
+    <section className="py-16 md:py-24">
+      <Contact />
+    </section>
+  );
+};
+
+export default ContactPage;
