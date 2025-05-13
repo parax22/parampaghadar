5baf546f40954dc4b14677d11cebf29ffb26ed45
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2025-05-13 16:07:35 +0530

    ± layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
 import BottomHero from "@/components/home/bottom-hero";
+import ContactSection from "@/components/home/contact-section";
 import Hero from "@/components/home/hero";
 import Projects from "@/components/home/projects";
 import WorkExperience from "@/components/home/work-experience";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "DevsPro | Portfolio",
+  description: "Created by devsloka.in ",
+};
 
 export default function Home() {
   return (
@@ -11,6 +18,7 @@ export default function Home() {
         <WorkExperience />
         <Projects />
         <BottomHero />
+        <ContactSection />
       </main>
     </>
   );
