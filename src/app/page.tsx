9b565902113f8e579c15cfd270ea5c31f51947a3
Paghadar Param <paghadarparam2@gmail.com>
2025-06-07 13:11:24 +0530

    Comment Import Line in page FIle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import BottomHero from "@/components/home/bottom-hero";
 import ContactSection from "@/components/home/contact-section";
 import Hero from "@/components/home/hero";
 import Projects from "@/components/home/projects";
-import WorkExperience from "@/components/home/work-experience";
+// import WorkExperience from "@/components/home/work-experience";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
