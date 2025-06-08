c4e89aa124b7f9c7c9848c5a3bc64950ce66f0b4
Paghadar Param <paghadarparam2@gmail.com>
2025-06-08 10:52:37 +0530

    Solve Ref Error in bottom-hero file

--- src/components/home/bottom-hero.tsx
+++ src/components/home/bottom-hero.tsx
@@ -17,17 +17,20 @@ export default function BottomHero() {
       },
       { threshold: 0.2 }
     );
-
-    if (sectionRef.current) {
-      observer.observe(sectionRef.current);
+  
+    const currentRef = sectionRef.current;
+  
+    if (currentRef) {
+      observer.observe(currentRef);
     }
-
+  
     return () => {
-      if (sectionRef.current) {
-        observer.unobserve(sectionRef.current);
+      if (currentRef) {
+        observer.unobserve(currentRef);
       }
     };
   }, []);
+  
 
   const skills = [
     "Front-end Development",
